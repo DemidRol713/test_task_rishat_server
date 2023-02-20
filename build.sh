f50be9671e5b4578fb969d499f139f3f56eeefb9
DemidRol713 <shatilov.demid@gmail.com>
2023-02-20 20:36:22 +0400

    fix bug

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
-pip3 install -r requirements.txt
+
 poetry install
 
 python manage.py collectstatic --no-input
