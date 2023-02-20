a09405c5a6e6fd39235d48a55aa5f86572e71632
DemidRol713 <shatilov.demid@gmail.com>
2023-02-20 20:51:04 +0400

    fix bug

--- test_task_rishat/wsgi.py
+++ test_task_rishat/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_task_rishat_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_task_rishat.settings')
 
 application = get_wsgi_application()
