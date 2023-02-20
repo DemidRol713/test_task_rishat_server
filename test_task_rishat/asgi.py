fbea93bdfc53a39be223253b669649cfff562fbf
DemidRol713 <shatilov.demid@gmail.com>
2023-02-20 20:55:34 +0400

    fix bug

--- test_task_rishat/asgi.py
+++ test_task_rishat/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_task_rishat_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_task_rishat.settings')
 
 application = get_asgi_application()
