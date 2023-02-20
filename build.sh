33a4bffb6f7fbe24868890cff6bfdc809730cec6
DemidRol713 <shatilov.demid@gmail.com>
2023-02-20 23:05:16 +0400

    fix bug

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 # exit on error
 set -o errexit
 
-/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+
 pip install pytz
 poetry install
-pip install stripe
+/opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
+/opt/render/project/src/.venv/bin/python -m pip install stripe
 #python -m pip install requests
 python manage.py collectstatic --no-input
 
