9ac22f8e739eacf657521aa51346bf6d4ab647e3
DemidRol713 <shatilov.demid@gmail.com>
2023-02-20 22:43:48 +0400

    fix bug

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -o errexit
 pip install pytz
 poetry install
 pip install stripe --no-binary :all:
-python -m pip install requests
+#python -m pip install requests
 python manage.py collectstatic --no-input
 
